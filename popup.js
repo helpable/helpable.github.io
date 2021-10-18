580713a9af18bbad0a31e1f484b2dea4869a6092
bkuermayr <45873859+bkuermayr@users.noreply.github.com>
2021-10-18 17:52:00 +0200

    initial commit

---
+++ popup.js
@@ -0,0 +1,28 @@
+
+//------
+// Teams
+//------
+
+
+function show_selected_team(event) {
+    let member = event.target.parentNode.parentNode.parentNode
+    console.log(member.querySelector('#name').value)
+
+
+    setTimeout(() => {
+        document.getElementById("infobox_team").style.display = "block"
+        console.log("here")
+    },1)
+
+    let team_name = document.getElementById("team_name").innerHTML = member.querySelector('#name').innerHTML;
+    let team_name_subtitle = document.getElementById("team_subtitle").innerHTML = member.querySelector('#role').innerHTML
+    let team_para = document.getElementById("team_para").innerHTML = member.querySelector('#text').innerHTML
+}
+
+
+document.getElementById("hide_on_click").addEventListener("click", () => {
+    document.getElementById("infobox_service").style.display = "none"
+    document.getElementById("infobox_team").style.display = "none"
+    document.getElementById("infobox_feature").style.display = "none"
+    console.log("hi");
+})
